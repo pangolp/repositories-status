d9abf66724a72369c4f796b5eea8aa5c61f344dc
Pagani Walter <paganiwalter@gmail.com>
2024-03-15 21:42:30 -0300

    Add a timer before searching

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,16 @@ new Vue({
     data () {
       return {
         repositories: 'no found',
+        loading: true
       }
     },
     mounted () {
-      axios
-        .get('https://api.github.com/orgs/azerothcore/repos?per_page=100;sort=updated;direction=desc')
-        .then(response => (this.repositories = response.data))
+        setTimeout(() => {
+            axios
+                .get('https://api.github.com/orgs/azerothcore/repos?per_page=100;sort=updated;direction=desc')
+                .then(response => {this.repositories = response.data})
+                .catch(error => {console.log(error)})
+                .finally(() => this.loading = false);
+        }, 10000)
     }
 })
